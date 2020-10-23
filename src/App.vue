a011bfc9aa1cb0df289e2e8266b48853862d7dfd
Ezra Ashenafi <eazash22@gmail.com>
2020-10-23 11:07:27 +0300

    bug fix: reduce chunkvendor.js file size

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,7 @@
   padding: 0;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
   text-align: center;
   color: #2c3e50;
 }
